fcc0b321eefade01970c9b884aad034636c327ce
Thysrael <thysrael@163.com>
2025-05-30 21:53:45 +0800

    feat(ALL): init config

---
+++ lua/keymaps.lua
@@ -0,0 +1,30 @@
+-- Clear highlights on search when pressing <Esc> in normal mode
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- Keybinds to make split navigation easier.
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Resize with arrows
+-- delta: 2 lines
+vim.keymap.set('n', '<C-down>', ':resize -2<CR>', opts)
+vim.keymap.set('n', '<C-up>', ':resize +2<CR>', opts)
+vim.keymap.set('n', '<C-right>', ':vertical resize -2<CR>', opts)
+vim.keymap.set('n', '<C-left>', ':vertical resize +2<CR>', opts)
+
+-- Highlight when yanking (copying) text
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
