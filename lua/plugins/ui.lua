15887a2d67e39cf492ade7d4f53c3542d8d3fdd6
Thysrael <thysrael@163.com>
2025-08-18 11:39:34 +0800

    feat(ui): add breadcrumbs

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -154,5 +154,36 @@ return {
             --   If not available, we use `mini` as the fallback
             "rcarriga/nvim-notify",
         }
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+        opts = { mode = "cursor", max_lines = 3 },
+    },
+    {
+        'Bekaboo/dropbar.nvim',
+        opts = {
+            bar = {
+                sources = function(buf, _)
+                    local sources = require('dropbar.sources')
+                    local utils = require('dropbar.utils')
+                    if vim.bo[buf].ft == 'markdown' then
+                        return {
+                            sources.markdown,
+                        }
+                    end
+                    if vim.bo[buf].buftype == 'terminal' then
+                        return {
+                            sources.terminal,
+                        }
+                    end
+                    return {
+                        utils.source.fallback({
+                            sources.lsp,
+                            sources.treesitter,
+                        }),
+                    }
+                end
+            },
+        },
     }
 }
