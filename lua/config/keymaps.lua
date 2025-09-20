e58a9b06fd0255853e00dae337a295b7c04bf0fe
Thysrael <thysrael@163.com>
2025-09-20 21:46:16 +0800

    chore(vscode): try vscode-neovim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,5 @@
 -- Clear highlights on search when pressing <Esc> in normal mode
-vim.keymap.set('n', 'esc', '<cmd>nohlsearch<CR>')
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
